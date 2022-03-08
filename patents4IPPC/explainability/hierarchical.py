9f69c0f17feb7dba82d3d83a03852475f50370d6
anferico <f.cariaggi4@gmail.com>
2022-03-08 12:17:34 +0100

    Only compute segment attributions for HierarchicalTransformer

--- patents4IPPC/explainability/hierarchical.py
+++ patents4IPPC/explainability/hierarchical.py
@@ -47,64 +47,48 @@ class HierarchicalTransformerTextSimilarityExplainer:
         normalize_attributions: bool = False
     ):
         (
-            text1_encoded_segments,
-            text2_encoded_segments,
-            baseline1_encoded_segments,
-            baseline2_encoded_segments
+            text1_segment_embeddings,
+            text2_segment_embeddings,
+            baseline1_segment_embeddings,
+            baseline2_segment_embeddings
         ) = self._construct_baselines_and_inputs(text1, text2)
         
-        # Explain text1 while keeping text2 freezed
+        # Explain text1 while keeping text2 frozen
         with torch.no_grad():
-            text2_embedding = self._embed_single_text(
-                text2_encoded_segments["input_ids"],
-                text2_encoded_segments["attention_mask"]
+            text2_embedding = self._get_document_embedding(
+                text2_segment_embeddings
             )
-        text1_token_attributions, text1_segment_attributions = \
+        text1_segment_attributions = \
             self._compute_attributions(
-                text1_encoded_segments["input_ids"],
-                baseline1_encoded_segments["input_ids"],
-                text1_encoded_segments["attention_mask"],
+                text1_segment_embeddings,
+                baseline1_segment_embeddings,
                 text2_embedding,
                 n_steps=n_steps,
                 internal_batch_size=internal_batch_size
             )
 
-        # Explain text2 while keeping text1 freezed
+        # Explain text2 while keeping text1 frozen
         with torch.no_grad():
-            text1_embedding = self._embed_single_text(
-                text1_encoded_segments["input_ids"],
-                text1_encoded_segments["attention_mask"]
+            text1_embedding = self._get_document_embedding(
+                text1_segment_embeddings
             )
-        text2_token_attributions, text2_segment_attributions = \
+        text2_segment_attributions = \
             self._compute_attributions(
-                text2_encoded_segments["input_ids"],
-                baseline2_encoded_segments["input_ids"],
-                text2_encoded_segments["attention_mask"],
+                text2_segment_embeddings,
+                baseline2_segment_embeddings,
                 text1_embedding,
                 n_steps=n_steps,
                 internal_batch_size=internal_batch_size
             )
 
         # Summarize and optionally normalize attributions
-        text1_token_attributions = self._summarize_attributions(
-            text1_token_attributions
-        )
-        text2_token_attributions = self._summarize_attributions(
-            text2_token_attributions
-        )
         text1_segment_attributions = self._summarize_attributions(
             text1_segment_attributions
         )
         text2_segment_attributions = self._summarize_attributions(
             text2_segment_attributions
-        )        
+        )
         if normalize_attributions:
-            text1_token_attributions = self._normalize_attributions(
-                text1_token_attributions
-            )
-            text2_token_attributions = self._normalize_attributions(
-                text2_token_attributions
-            )
             text1_segment_attributions = self._normalize_attributions(
                 text1_segment_attributions
             )
@@ -112,26 +96,15 @@ class HierarchicalTransformerTextSimilarityExplainer:
                 text2_segment_attributions
             )
 
-        text1_tokens = self.tokenizer.convert_ids_to_tokens(
-            text1_encoded_segments["input_ids"].detach().cpu().tolist()[0]
-        )
-        text2_tokens = self.tokenizer.convert_ids_to_tokens(
-            text2_encoded_segments["input_ids"].detach().cpu().tolist()[0]
-        )
-
         text1_segments = \
             text1 if isinstance(text1, list) else text1.split("[SEGMENT_SEP]")
         text2_segments = \
             text2 if isinstance(text2, list) else text2.split("[SEGMENT_SEP]")            
         return (
-            text1_tokens,
-            text1_token_attributions.detach().cpu().tolist()[0],
             text1_segments,
-            text1_segment_attributions.detach().cpu().tolist()[0],
-            text2_tokens,
-            text2_token_attributions.detach().cpu().tolist()[0],
+            text1_segment_attributions.detach().cpu().tolist(),
             text2_segments,
-            text2_segment_attributions.detach().cpu().tolist()[0]
+            text2_segment_attributions.detach().cpu().tolist()
         )
 
     def _construct_baselines_and_inputs(self, text1, text2):
@@ -146,6 +119,13 @@ class HierarchicalTransformerTextSimilarityExplainer:
             self.tokenizer,
             segment_transformer_max_length
         )
+        with torch.no_grad():
+            text1_segment_embeddings = self.model.get_segment_embeddings(
+                move_encoded_inputs_to_device(
+                    text1_encoded_segments, self.device
+                )
+            )
+
         text2_encoded_segments, _ = prepare_inputs_for_hierarchical_transformer(
             OrderedDict({
                 "text2": (text2 if isinstance(text2, list)
@@ -154,83 +134,98 @@ class HierarchicalTransformerTextSimilarityExplainer:
             self.tokenizer,
             segment_transformer_max_length
         )
+        with torch.no_grad():
+            text2_segment_embeddings = self.model.get_segment_embeddings(
+                move_encoded_inputs_to_device(
+                    text2_encoded_segments, self.device
+                )
+            )
 
-        baseline1_encoded_segments = deepcopy(text1_encoded_segments)
-        baseline1_encoded_segments["input_ids"].apply_(
-            lambda token_id:
-                token_id
-                if token_id in self.tokenizer.all_special_ids
-                else self.tokenizer.pad_token_id
+        baseline1_segment_embeddings = torch.zeros_like(
+            text1_segment_embeddings
         )
+        baseline2_segment_embeddings = torch.zeros_like(
+            text2_segment_embeddings
+        )
+
+        ################################################################
+        # TODO: Remove commented out code
+
+        # baseline1_encoded_segments = deepcopy(text1_encoded_segments)
+        # baseline1_encoded_segments["input_ids"].apply_(
+        #     lambda token_id:
+        #         token_id
+        #         if token_id in self.tokenizer.all_special_ids
+        #         else self.tokenizer.pad_token_id
+        # )
         
-        baseline2_encoded_segments = deepcopy(text2_encoded_segments)
-        baseline2_encoded_segments["input_ids"].apply_(
-            lambda token_id:
-                token_id
-                if token_id in self.tokenizer.all_special_ids
-                else self.tokenizer.pad_token_id
-        )
+        # baseline2_encoded_segments = deepcopy(text2_encoded_segments)
+        # baseline2_encoded_segments["input_ids"].apply_(
+        #     lambda token_id:
+        #         token_id
+        #         if token_id in self.tokenizer.all_special_ids
+        #         else self.tokenizer.pad_token_id
+        # )
 
-        def unsqueeze_encoded_inputs(encoded_inputs, dim=0):
-            return {k: v.unsqueeze(dim) for k, v in encoded_inputs.items()}
+        # def unsqueeze_encoded_inputs(encoded_inputs, dim=0):
+        #     return {k: v.unsqueeze(dim) for k, v in encoded_inputs.items()}
+        
+        # return (
+        #     move_encoded_inputs_to_device(
+        #         unsqueeze_encoded_inputs(text1_encoded_segments, dim=0),
+        #         self.device
+        #     ),
+        #     move_encoded_inputs_to_device(
+        #         unsqueeze_encoded_inputs(text2_encoded_segments, dim=0),
+        #         self.device
+        #     ),
+        #     move_encoded_inputs_to_device(
+        #         unsqueeze_encoded_inputs(baseline1_encoded_segments, dim=0),
+        #         self.device
+        #     ),
+        #     move_encoded_inputs_to_device(
+        #         unsqueeze_encoded_inputs(baseline2_encoded_segments, dim=0),
+        #         self.device
+        #     )
+        # )
+        ################################################################
         
         return (
-            move_encoded_inputs_to_device(
-                unsqueeze_encoded_inputs(text1_encoded_segments, dim=0),
-                self.device
-            ),
-            move_encoded_inputs_to_device(
-                unsqueeze_encoded_inputs(text2_encoded_segments, dim=0),
-                self.device
-            ),
-            move_encoded_inputs_to_device(
-                unsqueeze_encoded_inputs(baseline1_encoded_segments, dim=0),
-                self.device
-            ),
-            move_encoded_inputs_to_device(
-                unsqueeze_encoded_inputs(baseline2_encoded_segments, dim=0),
-                self.device
-            )
+            text1_segment_embeddings.unsqueeze(0).to(self.device),
+            text2_segment_embeddings.unsqueeze(0).to(self.device),
+            baseline1_segment_embeddings.unsqueeze(0).to(self.device),
+            baseline2_segment_embeddings.unsqueeze(0).to(self.device)
         )
 
-    def _embed_single_text(self, input_ids, attention_mask):
-        document_ids_and_n_segments = [
-            (f"text_{i}", input_ids.size()[1]) for i, _ in enumerate(input_ids)
-        ]
-        text_embedding = self.model(
-            {"input_ids": input_ids.reshape((-1, *input_ids.size()[2:])),
-             "attention_mask": attention_mask.reshape(-1, *attention_mask.size()[2:])},
-            document_ids_and_n_segments
+    def _get_document_embedding(self, segment_embeddings):
+        return self.model.get_document_embeddings(
+            segment_embeddings, [("text", segment_embeddings.size()[1])]
         )
-        return text_embedding
 
     def _compute_attributions(
         self,
-        input_ids,
-        baseline_ids,
-        attention_mask,
+        input_segment_embeddings,
+        baseline_segment_embeddings,
         other_text_embedding,
         n_steps=50,
         internal_batch_size=2
     ):
-        token_attributions = self._compute_token_attributions(
-            input_ids,
-            baseline_ids,
-            attention_mask,
+        self.model.zero_grad()
+
+        attribution_engine = LayerIntegratedGradients(
+            self._measure_cosine_similarity, self.model.document_embedder
+        )
+
+        segment_attributions = self._compute_integrated_gradients(
+            attribution_engine,
+            input_segment_embeddings,
+            baseline_segment_embeddings,
             other_text_embedding,
             n_steps,
             internal_batch_size
         )
-        segment_attributions = self._compute_segment_attributions(
-            input_ids,
-            baseline_ids,
-            attention_mask,
-            other_text_embedding,
-            n_steps,
-            internal_batch_size
-        )
-        
-        return token_attributions, segment_attributions
+
+        return segment_attributions   
 
     def _summarize_attributions(self, attributions):
         attributions = attributions.sum(dim=-1)
@@ -244,65 +239,12 @@ class HierarchicalTransformerTextSimilarityExplainer:
                                              / attribution_magnitudes.max())
         return signs * normalized_attribution_magnitudes   
 
-    def _compute_token_attributions(
-        self,
-        input_ids,
-        baseline_ids,
-        attention_mask,
-        other_text_embedding,
-        n_steps,
-        internal_batch_size
-    ):
-        self.model.zero_grad()
-
-        attribution_engine = LayerIntegratedGradients(
-            self._measure_cosine_similarity,
-            self.model.segment_transformer.embeddings.word_embeddings
-        )
-
-        token_attributions = self._compute_integrated_gradients(
-            attribution_engine,
-            input_ids,
-            baseline_ids,
-            attention_mask,
-            other_text_embedding,
-            n_steps,
-            internal_batch_size
-        )
-
-        return token_attributions     
-
-    def _compute_segment_attributions(
-        self,
-        input_ids,
-        baseline_ids,
-        attention_mask,
-        other_text_embedding,
-        n_steps,
-        internal_batch_size
-    ):
-        self.model.zero_grad()
-
-        attribution_engine = LayerIntegratedGradients(
-            self._measure_cosine_similarity, self.model.document_embedder
-        )
-
-        segment_attributions = self._compute_integrated_gradients(
-            attribution_engine,
-            input_ids,
-            baseline_ids,
-            attention_mask,
-            other_text_embedding,
-            n_steps,
-            internal_batch_size
-        )
-
-        return segment_attributions
-
     def _measure_cosine_similarity(
-        self, input_ids, attention_mask, other_text_embedding
+        self, input_segment_embeddings, other_text_embedding
     ):
-        input_embedding = self._embed_single_text(input_ids, attention_mask)
+        input_embedding = self._get_document_embedding(
+            input_segment_embeddings
+        )
         return torch.cosine_similarity(
             input_embedding, other_text_embedding, dim=0
         )
@@ -310,23 +252,22 @@ class HierarchicalTransformerTextSimilarityExplainer:
     def _compute_integrated_gradients(
         self,
         attribution_engine,
-        input_ids,
-        baseline_ids,
-        attention_mask,
+        input_segment_embeddings,
+        baseline_segment_embeddings,
         other_text_embedding,
         n_steps,
-        internal_batch_size        
+        internal_batch_size
     ):
         self._maybe_disable_cudnn()
         # ^ If the `document_embedder` module of the model is
         #   recurrence-based, we must disable CUDNN because computing
         #   gradients for a recurrent layer is not supported when said
-        #   layer is in "eval" mode
+        #   layer is in "eval()" mode
         # (https://github.com/pytorch/captum/blob/master/docs/faq.md#how-can-i-resolve-cudnn-rnn-backward-error-for-rnn-or-lstm-network)
         attributions, convergence_delta = attribution_engine.attribute(
-            inputs=input_ids,
-            baselines=baseline_ids,
-            additional_forward_args=(attention_mask, other_text_embedding),
+            inputs=input_segment_embeddings,
+            baselines=baseline_segment_embeddings,
+            additional_forward_args=(other_text_embedding,),
             n_steps=n_steps,
             internal_batch_size=internal_batch_size,
             return_convergence_delta=True
